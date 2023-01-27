b7455fedae209388ab18a9e3738738df8c7591a6
song-eujin <songuijin01@gmail.com>
2023-01-27 13:54:42 +0900

    1

--- src/components/axios/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const customAxios = axios.create({
-    baseURL:"http://192.168.0.42:8080"
-});
-
-export default customAxios;
--- src/views/CRM/Starter.js
+++
@@ -1,5 +0,0 @@
-const Starter = () => {
-  return <div>CRM 시작페이지222</div>;
-};
-
-export default Starter;
